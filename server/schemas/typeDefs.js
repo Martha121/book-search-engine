71bfcc1551b24f757bc1afce40941f61556b2c2c
MGcommits <marthagamez211@gmail.com>
2021-05-14 13:34:25 -0500

    Implement Apollo Server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
