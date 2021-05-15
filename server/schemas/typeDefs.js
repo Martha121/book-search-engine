3a8f16ef1d4f9275839ddb318d634d83d42ed4f8
MGcommits <marthagamez211@gmail.com>
2021-05-15 08:47:31 -0500

    Create Schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,6 +11,20 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
   type Query {
     me: User
   }
