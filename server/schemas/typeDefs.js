e91bcdcad0e6eac728a42e5a8c13bf19f8c58f64
MGcommits <marthagamez211@gmail.com>
2021-05-15 16:35:07 -0500

    Create resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,12 @@
-// import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
-// create our typeDefs
 const typeDefs = gql`
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
   type User {
     _id: ID!
     username: String!
@@ -10,7 +14,6 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
-
   type Book {
     bookId: ID!
     authors: [String]
@@ -19,16 +22,21 @@ const typeDefs = gql`
     link: String
     title: String!
   }
-
-  type Auth {
-    token: ID
-    user: User
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
   }
-
   type Query {
     me: User
   }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const { authMiddleware } = require("./utils/auth");
+//***no longer nedds*** const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
